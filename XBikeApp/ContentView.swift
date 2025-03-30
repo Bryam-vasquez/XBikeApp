2c19231f5217afcc9bf2514b93f0b2ca73ed21f1
Bryam-vasquez <bryam.vasquez.ch@gmail.com>
2025-03-29 21:06:53 -0500

    Add base project

---
+++ XBikeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XBikeApp
+//
+//  Created by Juan Diego Olivas Maldonado on 29/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XBikeApp/XBikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XBikeAppApp.swift
+//  XBikeApp
+//
+//  Created by Juan Diego Olivas Maldonado on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XBikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
